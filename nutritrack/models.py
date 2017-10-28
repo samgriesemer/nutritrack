663ad5189f69d5d9fdaf9c5364424eb879fa3190
Connor Monahan <cma2714@gmail.com>
2017-10-28 14:53:13 -0500

    Models

--- nutritrack/models.py
+++ nutritrack/models.py
@@ -15,12 +15,29 @@ class Nutrient(models.Model):
     iron = models.FloatField()
     calcium = models.FloatField()
 
+    def __str__(self):
+        return f'{self.kcal} kcal, {self.fat} g total fat, {self.carb} g total carbohydrate, {self.sugar} g sugars, ' \
+               f'{self.protein} g protein, {self.sodium} mg sodium, {self.vA}% Vitamin A, {self.vC}% Vitamin C, ' \
+               f'{self.iron}% Iron, {self.calcium}% Calcium '
+
 
 class Meal(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256, default='')
+
+    @property
+    def ingredients(self):
+        return Ingredient.objects.filter(meal=self)
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256, default='')
     amount = models.FloatField()
     nutrients = models.ForeignKey(Nutrient)
+
+    def __str__(self):
+        return f'{self.name} - {self.amount} g'
